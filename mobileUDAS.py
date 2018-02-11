e5df1dd7fe66c536d3e5724cc6e1b1d1c755e551
Patrick <tconnolly@mlml.calstate.edu>
2018-02-10 18:21:18 -0700

    function for reading raw data

--- mobileUDAS.py
+++ mobileUDAS.py
@@ -2,18 +2,25 @@ import serial, time, csv
 from DateTime import DateTime
 from datetime import datetime as dt
 
-
-
 #Helper Functions
 def getTime():
-    #This will give us the time in unix, UTC, and local
+    '''This will give us the time in unix, UTC, and local'''
     unix = time.time() #returns unix in utc
     utc = DateTime(unix,'UTC')
     local = DateTime(unix,'US/Pacific')
     utc_iso = utc.ISO8601()
-    
     return (unix,local,utc_iso)
-    
+
+def getRaw(sensor):
+    ''' Sequence of steps to try reading serial data from a sensor'''
+    try:
+        data_raw = sensor.readline().decode() # Python 3
+    except:
+        try:
+            data_raw = sensor.readline() #Python 2
+        except:
+            data_raw = ''
+    return data_raw
     
 def parseSCUFA(raw):
     try:
@@ -23,7 +30,6 @@ def parseSCUFA(raw):
         correctedFL = float(parsed[3])
         turb = float(parsed[4])
         temp = float(parsed[5][:-3])
-        
         return (rawFL,correctedFL,turb,temp)
     except:
         print('ERROR: Invalid SCUFA Data: ',raw)
@@ -35,9 +41,7 @@ def parseTSG(raw):
         temp = float(parsed[0])
         cond = float(parsed[1])
         sal = float(parsed[2])
-    
         return (temp,cond,sal)
-        
     except:
         print('ERROR: Invalid TSG data: ',raw)
         return ('NaN','NaN','NaN')
@@ -46,40 +50,41 @@ def parseTrans(raw):
     try:
         parsed = raw.split('\t')
         ba = float(parsed[4])
-        
-    
         return ba
-        
     except:
         print('ERROR: Invalid Trans data: ',raw)
         return 'NaN'
-
-#Connect to each sensor via the com port its connected to..
-#If you switch around the plugs, you're going to have to..
-#.. update the 'COMX' value accordingly
-
-#scufa = serial.Serial('COM7',9600,timeout=10)
-#tsg   = serial.Serial('COM9',38400,timeout=10)
-#trans = serial.Serial('COM8',19200,timeout=10)
 
 parse_scufa = False
 parse_tsg = True
 parse_trans = True
 
+#Connect to each sensor via the com port its connected to..
+#If you switch around the plugs, you're going to have to..
+#.. update the 'ttyUSBx' value accordingly
+# to find ports on Raspberry Pi: dmesg | grep tty
+# need to figure out how to make these consistent
 scufa_port = ''
-tsg_port = '/dev/ttyUSB0'
-trans_port = '/dev/ttyUSB1'
+tsg_port = '/dev/ttyUSB2'
+trans_port = '/dev/ttyUSB0'
 
 scufa_baud = 9600
 tsg_baud = 38400
 trans_baud = 19200
 
 if parse_scufa:
-    scufa = serial.Serial(scufa_port,scufa_baud,timeout=10)
+    scufa = serial.Serial(scufa_port,scufa_baud,timeout=2)
+else:
+    scufa = []
 if parse_tsg:
-    tsg   = serial.Serial(tsg_port,tsg_baud,timeout=10)
+    tsg   = serial.Serial(tsg_port,tsg_baud,timeout=2)
+else:
+    tsg = []
+    
 if parse_trans:
-    trans = serial.Serial(trans_port,trans_baud,timeout=10)
+    trans = serial.Serial(trans_port,trans_baud,timeout=2)
+else:
+    trans = []
 
 t = getTime()[1]
 dateString = t.strftime('%Y%m%d-%H%M')
@@ -102,35 +107,15 @@ header = ['unix_time','local_time','utc_time','sbe_temp','sbe_cond',
           'sbe_sal','scufa_RF','scufa_CF','scufa_turb','scufa_temp','ba']
 writer.writerow(header)
 
-
 #Loop runs until user stops it
 while True:
     try:
         t = getTime()
+
+        scufa_data_raw = getRaw(scufa)
+        tsg_data_raw = getRaw(tsg)
+        trans_data_raw = getRaw(trans)
         
-        try:
-            scufa_data_raw = scufa.readline().decode()
-        except:
-            try:
-                scufa_data_raw = scufa.readline()
-            except:
-                scufa_data_raw = ''
-            
-        try:
-            tsg_data_raw = tsg.readline().decode()
-        except:
-            try:
-                tsg_data_raw = scufa.readline()
-            except:
-                tsg_data_raw = ''    
-        try:
-            trans_data_raw = trans.readline().decode()
-        except:
-            try:
-                trans_data_raw = scufa.readline()
-            except:
-                trans_data_raw = ''
-       
         print('Time: ',t)
         print('SCUFA_raw: ',scufa_data_raw)
         print('TSG_raw: ',tsg_data_raw)
